19fc9de105808ca9a620ad40fdf3d86eba517056
yyymmmccc <jimindong9814@gmail.com>
2024-10-26 18:37:17 +0900

    MyReviewListResponseDto 게임 이미지 추가

--- src/main/java/com/project/game/dto/response/review/MyReviewListResponseDto.java
+++ src/main/java/com/project/game/dto/response/review/MyReviewListResponseDto.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -15,28 +16,29 @@ import org.hibernate.annotations.UpdateTimestamp;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Slf4j
 public class MyReviewListResponseDto {
 
     private int reviewId;
-    private String profileImage;
     private String nickname;
     private int rating;
     private String content;
     private String updatedDate;
     private int gameId;
+    private String gameImage;
     private String gameName;
 
     public static MyReviewListResponseDto of(ReviewEntity reviewEntity){
 
         return MyReviewListResponseDto.builder()
                 .reviewId(reviewEntity.getReviewId())
-                .profileImage(reviewEntity.getUserEntity().getProfileImage())
                 .nickname(reviewEntity.getUserEntity().getNickname())
-                .gameId(reviewEntity.getGameEntity().getGameId())
-                .gameName(reviewEntity.getGameEntity().getGameName())
                 .rating(reviewEntity.getRating())
                 .content(reviewEntity.getContent())
                 .updatedDate(reviewEntity.getUpdatedDate())
+                .gameId(reviewEntity.getGameEntity().getGameId())
+                .gameImage(reviewEntity.getGameEntity().getGameImageEntityList().get(0).getGameImageUrl())
+                .gameName(reviewEntity.getGameEntity().getGameName())
                 .build();
     }
 
